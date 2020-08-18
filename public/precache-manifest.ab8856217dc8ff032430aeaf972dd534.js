05d9b1562945063308e3c815158f36933d40e96e
Rgaliant <ryanwfgale@gmail.com>
2020-08-17 21:38:04 -0600

    add in react front end

---
+++ public/precache-manifest.ab8856217dc8ff032430aeaf972dd534.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e60495f9d327c9138f552003c01ca17c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "faa32aeb2929baf6d884",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fc03aed87d75d818f89c",
+    "url": "/static/js/2.09f1b02a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.09f1b02a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "faa32aeb2929baf6d884",
+    "url": "/static/js/main.02e8e279.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
